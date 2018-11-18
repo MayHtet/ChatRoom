3750f995e7e476455adf3ca3da1083b0f7b4c039
Pearl <pearlyatimaung97@gmail.com>
2018-11-18 14:55:26 +0630

    Auto stash before merge of "master" and "origin/master"

---
+++ ChatRoomApplication/app/src/main/java/com/example/mizuki/chatroomapplication/AppModel.java
@@ -0,0 +1,11 @@
+package com.example.mizuki.chatroomapplication;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.support.annotation.NonNull;
+
+public class AppModel extends AndroidViewModel {
+    public AppModel(@NonNull Application application) {
+        super(application);
+    }
+}
---
+++ ChatRoomApplication/app/src/main/java/com/example/mizuki/chatroomapplication/NetworkConstant.java
@@ -0,0 +1,5 @@
+package com.example.mizuki.chatroomapplication;
+
+public class NetworkConstant {
+    public static final String CHAT_MESSAGE =
+}
