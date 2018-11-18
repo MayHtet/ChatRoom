5f83aef8df4db98fb49a1532cabd203ca54fa734
Pearl <pearlyatimaung97@gmail.com>
2018-11-18 15:45:31 +0630

    Auto stash before merge of "master" and "origin/master"

--- ChatRoomApplication/app/src/main/java/com/example/mizuki/chatroomapplication/activities/MainActivity.java
+++ ChatRoomApplication/app/src/main/java/com/example/mizuki/chatroomapplication/activities/MainActivity.java
@@ -1,25 +1,47 @@
 package com.example.mizuki.chatroomapplication.activities;
 
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.mizuki.chatroomapplication.R;
 
+<<<<<<< Updated upstream
+=======
+import java.util.List;
+
+>>>>>>> Stashed changes
 public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+<<<<<<< Updated upstream
 
     }
 
     @Override
     protected boolean setDisplayHomeEnabled() {
         return false;
+=======
+        setContentView(R.layout.activity_main);
+        getmAppModel().startLoadingNoteList(new NoteListCallBack() {
+            @Override
+            public void loadNoteList(List<ContactsContract.CommonDataKinds.Note> noteList) {
+
+                setupRecyclerView(noteList);
+                //Toast.makeText(MainActivity.this,"Size is "+noteList.size(),Toast.LENGTH_SHORT).show();
+            }
+        });
+>>>>>>> Stashed changes
     }
 
     @Override
     public int getLayout() {
+<<<<<<< Updated upstream
         return R.layout.activity_main;
+=======
+        return 0;
+>>>>>>> Stashed changes
     }
 }
