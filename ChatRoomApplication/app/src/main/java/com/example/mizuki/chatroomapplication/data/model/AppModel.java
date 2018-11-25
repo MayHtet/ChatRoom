1839cbe71a3494aa6fd576b76dedfce96927fd2b
Unknown <mayhtethlaingmh318@gmail.com>
2018-11-25 15:14:03 +0630

    change xml
    change xml

--- ChatRoomApplication/app/src/main/java/com/example/mizuki/chatroomapplication/data/model/AppModel.java
+++ ChatRoomApplication/app/src/main/java/com/example/mizuki/chatroomapplication/data/model/AppModel.java
@@ -3,31 +3,10 @@ import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.support.annotation.NonNull;
 import com.example.mizuki.chatroomapplication.data.vos.ChatRoom;
-<<<<<<< HEAD
-import com.example.mizuki.chatroomapplication.delegate.ChatListCallBack;
-import com.example.mizuki.chatroomapplication.utils.NetworkConstant;
 
 import com.example.mizuki.chatroomapplication.delegate.ChatListCallBack;
 import com.example.mizuki.chatroomapplication.utils.NetworkConstant;
 
-import com.example.mizuki.chatroomapplication.delegate.ChatListCallBack;
-import com.example.mizuki.chatroomapplication.utils.NetworkConstant;
-import com.example.mizuki.chatroomapplication.delegate.ChatListCallBack;
-import com.example.mizuki.chatroomapplication.utils.NetworkConstant;
-import com.example.mizuki.chatroomapplication.delegate.ChatListCallBack;
-import com.example.mizuki.chatroomapplication.utils.NetworkConstant;
-import com.example.mizuki.chatroomapplication.delegate.ChatListCallBack;
-import com.example.mizuki.chatroomapplication.utils.NetworkConstant;
-import com.example.mizuki.chatroomapplication.delegate.ChatListCallBack;
-import com.example.mizuki.chatroomapplication.utils.NetworkConstant;
-=======
-<<<<<<< HEAD
-=======
->>>>>>> master
-
->>>>>>> master
-import com.example.mizuki.chatroomapplication.delegate.ChatListCallBack;
-import com.example.mizuki.chatroomapplication.utils.NetworkConstant;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
